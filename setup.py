ff693602492886dc6b157ebef632891a65205dec
Rohit <tprohit9@gmail.com>
2021-02-09 18:26:02 +0530

    added MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
-setuptools.setup(
+setup(
 	name="gramup",
-	version="0.0.4",
+	version="0.0.5",
 	author="Rohit T P",
 	author_email="tprohit9@gmail.com",
 	description="A utility to use Telegram as a backup solution.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/rohittp0/GramUp",
-	packages=setuptools.find_packages(),
+	packages=["GramUP"],
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: GNU Affero General Public License v3",
