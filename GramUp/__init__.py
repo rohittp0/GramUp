4ba9120184972a9c42785d39b1785e6b43723034
Rohit <tprohit9@gmail.com>
2021-02-09 21:33:26 +0530

    Restructured project

--- GramUp/__init__.py
+++
@@ -1,1 +0,0 @@
-# init file for the program.
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
 setup(
 	name="gramup",
-	version="0.0.6",
+	version="0.0.5",
 	author="Rohit T P",
 	author_email="tprohit9@gmail.com",
 	description="A utility to use Telegram as a backup solution.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/rohittp0/GramUp",
-	packages=["GramUP"],
+	packages=find_packages("gramup"),
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: GNU Affero General Public License v3",
@@ -21,7 +21,7 @@ setup(
 	include_package_data=True,
 	install_requires=[ "speedtest-cli", "python-telegram" ],
 	entry_points={
-		"console_scripts": [ "gramup=GramUp.main:main" ]
+		"console_scripts": [ "gramup=gramup.main:main" ]
 	},
 	python_requires='>=3.6',
 )
