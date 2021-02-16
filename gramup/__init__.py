a8b76c351e866df43b4a7b920ab7b064ceacd6d5
Rohit <tprohit9@gmail.com>
2021-02-16 17:08:03 +0530

    Fixed pylint errors.

--- gramup/__init__.py
+++ gramup/__init__.py
@@ -1,3 +1,6 @@
+'''
+	This file stores the version information for GramUp.
+'''
 __version__ = '1.1.0'
 
 VERSION = __version__
--- gramup/__main__.py
+++ gramup/__main__.py
@@ -1,60 +1,68 @@
-#    This is a utility to use Telegram's unlimited storage for backup. 	
-#    Copyright (C) 2021  Rohit T P
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see https://www.gnu.org/licenses/
+'''
+    This is a utility to use Telegram's unlimited storage for backup.
+    Copyright (C) 2021  Rohit T P
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see https://www.gnu.org/licenses/
+'''
 
 try:
 	from login import login
 	from backup import backup
 	from restore import restore
 	from __init__ import __version__
-except : 	
+except ImportError:
 	from .login import login
 	from .backup import backup
 	from .restore import restore
 	from .__init__ import __version__
 
-def clientReady(tg,chat_id,bup_folders) :
-	choise = input("Backup (b) or Restore (r) ? : ")        
-	
+def client_ready(tg_client,chat_id,bup_folders) :
+	'''
+		This function is called once required data is
+		loaded and Telegram client is initalised.
+	'''
+	choise = input("Backup (b) or Restore (r) ? : ")
+
 	if choise == "b" :
-		backup(tg,chat_id,bup_folders)
+		backup(tg_client,chat_id,bup_folders)
 	elif choise == "r" :
-		restore(tg,chat_id)
+		restore(tg_client,chat_id)
 	else :
 		print("Invalid option")
 
 def main() :
+	'''
+		This function is called to start GramUp.
+	'''
 	banner = [
-		f"\n\n"
-		f"   █████████                                      █████  █████  v{__version__}",      
-		f"  ███░░░░░███                                    ░░███  ░░███",
-		f" ███     ░░░  ████████   ██████   █████████████   ░███   ░███  ████████",
-		f"░███         ░░███░░███ ░░░░░███ ░░███░░███░░███  ░███   ░███ ░░███░░███",
-		f"░███    █████ ░███ ░░░   ███████  ░███ ░███ ░███  ░███   ░███  ░███ ░███",
-		f"░░███  ░░███  ░███      ███░░███  ░███ ░███ ░███  ░███   ░███  ░███ ░███",
-		f" ░░█████████  █████    ░░████████ █████░███ █████ ░░████████   ░███████", 
-		f"  ░░░░░░░░░  ░░░░░      ░░░░░░░░ ░░░░░ ░░░ ░░░░░   ░░░░░░░░    ░███░░░",  
-		f"                                                               ░███",     
-		f"                          KEEP YOUR FILES SAFE                 █████",    
-		f"                                                              ░░░░░\n\n"
+		"\n\n"
+		f"   █████████                                      █████  █████  v{__version__}",
+		"  ███░░░░░███                                    ░░███  ░░███",
+		" ███     ░░░  ████████   ██████   █████████████   ░███   ░███  ████████",
+		"░███         ░░███░░███ ░░░░░███ ░░███░░███░░███  ░███   ░███ ░░███░░███",
+		"░███    █████ ░███ ░░░   ███████  ░███ ░███ ░███  ░███   ░███  ░███ ░███",
+		"░░███  ░░███  ░███      ███░░███  ░███ ░███ ░███  ░███   ░███  ░███ ░███",
+		" ░░█████████  █████    ░░████████ █████░███ █████ ░░████████   ░███████",
+		"  ░░░░░░░░░  ░░░░░      ░░░░░░░░ ░░░░░ ░░░ ░░░░░   ░░░░░░░░    ░███░░░",
+		"                                                               ░███",
+		"                          KEEP YOUR FILES SAFE                 █████",
+		"                                                              ░░░░░\n\n"
 	]
-	for line in banner : 
+	for line in banner :
 		print(line)
-	
-	login(clientReady)	
-    		    
-if __name__ == "__main__" :
-	main()   
 
+	login(client_ready)
+
+if __name__ == "__main__" :
+	main()
