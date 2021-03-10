e1bf60194b3560d3b0bd8515d6399d3b9522a628
Rohit <tprohit9@gmail.com>
2021-03-10 16:33:10 +0530

    Added build script.

---
+++ build.sh
@@ -0,0 +1,11 @@
+rm -rf dist
+
+pip install -r requirements.txt
+
+pyinstaller --clean -y -F -n "$1" \
+	--add-data="lib/libtdjson.so:telegram/lib/linux" \
+	--add-data="lib/libtdjson.dylib:telegram/lib/darwin" \
+	--icon="GramUp Icon.ico" \
+	--paths="gramup" gramup/__main__.py 
+
+rm -rf build "${1}.spec"	
