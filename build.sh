0e1d854ddf46e8ed41bfa79911739461b6de73c6
Rohit <tprohit9@gmail.com>
2021-03-10 23:16:13 +0530

    Cleaned branch

--- build.sh
+++
@@ -1,9 +0,0 @@
-pip install -r requirements.txt
-
-pyinstaller --clean -y -F -n "$1" \
-	--add-data="lib/libtdjson.so:telegram/lib/linux" \
-	--add-data="lib/libtdjson.dylib:telegram/lib/darwin" \
-	--icon="GramUp Icon.ico" \
-	--paths="gramup" gramup/__main__.py 
-
-rm -rf build "${1}.spec"	
--- commit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -name "*.py" -exec sed -i 's/[ \t]*$//' {} +
-pylint --rcfile='.pylintrc' 'gramup' 'setup.py' && git commit .
