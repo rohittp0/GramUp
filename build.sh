704e8a23c0bef0633ffbb2f00cbf71c8c8b166e2
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-03-11 23:17:09 +0530

    Added action to upload artifact to new release. (#20)
    * Redesigned UI.
    
    * Added settings menu
    
    * Added settings menu
    
    * Added error logging.
    
    * Bug fixes
    
    * Added in terminal folder chooser.
    
    * Improved caching.
    
    * Fixed typo.
    
    * Added Icon.
    
    * Added Icon to README
    
    * Croped icon.
    
    * Update README.md
    
    * Update README.md
    
    * Icon redisigned.
    
    * Changed icon path in README
    
    * Added build action
    
    * Fixed typo in build action.
    
    * Bug fix in build action.
    
    * Added caching in actions
    
    * Fixed typo
    
    * Upload files only after all jobs have finished.
    
    * Set build action to wait for files.
    
    * Add artifacts to release using actions

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-pip install -r requirements.txt
-
 pyinstaller --clean -y -F -n "$1" \
 	--add-data="lib/libtdjson.so:telegram/lib/linux" \
 	--add-data="lib/libtdjson.dylib:telegram/lib/darwin" \
 	--icon="GramUp Icon.ico" \
 	--paths="gramup" gramup/__main__.py 
 
-rm -rf build "${1}.spec"	
+rm -rf build "${1}.spec" && echo "Removed unwanted files"	
