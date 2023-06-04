f19eda510d193e07f70d010297051124442cbc0a
Rohit T P <tprohit9@gmail.com>
2023-06-04 22:10:05 +0530

    cors

--- gramup/main.py
+++ gramup/main.py
@@ -1,11 +1,9 @@
 import asyncio
 
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from starlette.requests import Request
-from starlette.responses import FileResponse
+from fastapi import FastAPI, HTTPException
+from starlette import status
+from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
 from starlette.websockets import WebSocket
 from telethon import TelegramClient
 
@@ -14,16 +12,24 @@ from constants import API_ID, API_HASH
 client = TelegramClient('anon', API_ID, API_HASH)
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
-icons_path = "static/icons"
 
-templates = Jinja2Templates(directory="templates")
-
-
-@app.websocket("/ws")
+@app.websocket("/api/ws/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+
+    if not client.is_connected():
+        await client.connect()
+
+    if await client.is_user_authorized():
+        return await websocket.send_json({"type": "connection", "status": "connected"})
 
     qr = await client.qr_login()
     await websocket.send_json({"url": qr.url, "type": "qr"})
@@ -41,34 +47,12 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.close()
 
 
-@app.get("/sw.js")
-def get_sw():
-    return FileResponse("static/js/sw.js", media_type="text/javascript")
-
-
-@app.get("/manifest.json")
-def get_manifest():
-    return FileResponse("static/manifest.json", media_type="application/json")
-
-
-@app.get("/login")
-async def login(request: Request):
-    if not client.is_connected():
-        await client.connect()
-
-    if await client.is_user_authorized():
-        return RedirectResponse(url="/", status_code=302)
-
-    context = {"request": request}
-
-    return templates.TemplateResponse("login.html", context=context)
-
-
-@app.get("/{folder:path}")
-async def files(request: Request, folder="", error=""):
+@app.get("/api/files/")
+async def files(path=""):
     if not client.is_connected() or not await client.is_user_authorized():
-        return RedirectResponse(url="/login", status_code=302)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not authorized")
 
-    context = {}
+    return []
 
-    return templates.TemplateResponse("files.html", context=context)
+
+app.mount("/", StaticFiles(directory="static"), name="static")
